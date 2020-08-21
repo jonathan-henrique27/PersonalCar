9a093f9c337108450686e73f07e67da1fb5b4e72
Jonathan Henrique <jonathan.henrique@rikai.com.br>
2020-08-21 16:43:48 -0300

    centro de custos alteração

--- PersonalCar/Controllers/CentroDeCustosController.cs
+++ PersonalCar/Controllers/CentroDeCustosController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using PersonalCar.Data;
 using PersonalCar.Models.Domains;
 
