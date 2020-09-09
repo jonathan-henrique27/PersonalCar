5dd0c40a4b2d5549f257d972e72bbd3babfc3c22
brunamelissavaz <bruna.melissa@icloud.com>
2020-09-09 17:04:09 -0300

    passageiros controller criado

---
+++ PersonalCar/Controllers/PassageirosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PersonalCar.Data;
+using PersonalCar.Models.Domains;
+
+namespace PersonalCar.Controllers
+{
+    public class PassageirosController : Controller
+    {
+        private readonly PersonalCarContext _context;
+
+        public PassageirosController(PersonalCarContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Passageiros
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Passageiro.ToListAsync());
+        }
+
+        // GET: Passageiros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var passageiro = await _context.Passageiro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (passageiro == null)
+            {
+                return NotFound();
+            }
+
+            return View(passageiro);
+        }
+
+        // GET: Passageiros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Passageiros/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Celular")] Passageiro passageiro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(passageiro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(passageiro);
+        }
+
+        // GET: Passageiros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var passageiro = await _context.Passageiro.FindAsync(id);
+            if (passageiro == null)
+            {
+                return NotFound();
+            }
+            return View(passageiro);
+        }
+
+        // POST: Passageiros/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Celular")] Passageiro passageiro)
+        {
+            if (id != passageiro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(passageiro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PassageiroExists(passageiro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(passageiro);
+        }
+
+        // GET: Passageiros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var passageiro = await _context.Passageiro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (passageiro == null)
+            {
+                return NotFound();
+            }
+
+            return View(passageiro);
+        }
+
+        // POST: Passageiros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var passageiro = await _context.Passageiro.FindAsync(id);
+            _context.Passageiro.Remove(passageiro);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PassageiroExists(int id)
+        {
+            return _context.Passageiro.Any(e => e.Id == id);
+        }
+    }
+}
